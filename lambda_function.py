b0ee34e122c2ffeeda0d6784455b63ff379a9cc1
Simon Prickett <simon@Simons-MacBook-Pro.local>
2016-07-12 08:01:44 -0700

    Added missing session end function.

--- lambda_function.py
+++ lambda_function.py
@@ -57,6 +57,13 @@ def get_welcome_response():
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
+def handle_session_end_request():
+    card_title = "BART - Thanks"
+    speech_output = "Thank you for using the BART skill.  See you next time!"
+    should_end_session = True
+
+    return build_response({}, build_speechlet_response(card_title, speech_output, None, should_end_session))
+
 def get_system_status():
     session_attributes = {}
     card_title = "BART System Status"
